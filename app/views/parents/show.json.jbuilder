c1234445418d56c2e5ac0c7ff78223683f11c94b
Prelang Builder <builder@prelang.com>
2015-09-09 12:52:38 +0000

    Generated scaffolding for model 'Parent' via Rails

---
+++ app/views/parents/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @parent, :id, :fdasa, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :parents
+
   resources :childdfs
 
   resources :childdssses
